d2f00834210de220204bfe2dd545ed6ca5b82d4d
ArchiBC <archi-bc@outlook.com>
2025-08-14 18:05:35 +0800

    feat(Interval): 添加区间连接和集合运算功能
    - 增加 IsConnect 方法判断两个区间是否连接
    - 添加 Union 方法实现区间合并
    - 添加 Intersect 方法实现区间交集
    - 优化 Reverse 方法，交换区间同时交换包含性
    - 重命名 Contains 为 IsContains，提高方法命名一致性

--- Sunlight-shading-interval-algorithm/IntervalLib/Interval.cs
+++ Sunlight-shading-interval-algorithm/IntervalLib/Interval.cs
@@ -27,6 +27,7 @@ public struct Interval<T>(T start, T end, bool containsStart = true, bool contai
     public void Reverse()
     {
         (_start, _end) = (_end, _start);
+        (_containsStart, _containsEnd) = (_containsEnd, _containsStart);
     }
     
     public static Interval<T> Order(Interval<T> a)
@@ -34,7 +35,7 @@ public struct Interval<T>(T start, T end, bool containsStart = true, bool contai
         return a.IsOrder ? a : new Interval<T>(a.End, a.Start, a.ContainsStart,a.ContainsEnd);
     }
     
-    public static bool Contains(Interval<T> interval, T value)
+    public static bool IsContains(Interval<T> interval, T value)
     {
         var order = Order(interval);
         if (value < order.Start || value > order.End ) return false;
@@ -44,12 +45,53 @@ public struct Interval<T>(T start, T end, bool containsStart = true, bool contai
         return false;
     }
     
-    
-
     public static Interval<T> Reverse(Interval<T> interval)
     {
         return new Interval<T>(interval.End, interval.Start);
     }
+    public static bool IsConnect (Interval<T> a, Interval<T> b)
+    {
+        var orderA = Order(a);
+        var orderB = Order(b);
+        var containsA = IsContains(orderA, orderB.Start) || IsContains(orderA, orderB.End);
+        var containsB = IsContains(orderB, orderA.Start) || IsContains(orderB, orderA.End);
+        return containsA || containsB;
+    }
+    
+    public static Interval<T> Union (Interval<T> a, Interval<T> b)
+    {
+        var orderA = Order(a);
+        var orderB = Order(b);
+        if (!IsConnect(orderA, orderB))
+        {
+            throw new Exception("区间未连接，无法合并");
+        }
+        
+        return new Interval<T>(
+            orderA.Start<orderB.Start ? orderA.Start : orderB.Start, 
+            orderB.End<orderA.End ? orderA.End : orderB.End,
+            orderA.Start<orderB.Start ? orderA.ContainsStart : orderB.ContainsStart,
+            orderB.End<orderA.End ? orderA.ContainsEnd : orderB.ContainsEnd
+            );
+    }
+    
+    public static Interval<T> Intersect (Interval<T> a, Interval<T> b)
+    {
+        var orderA = Order(a);
+        var orderB = Order(b);
+        if (!IsConnect(orderA, orderB))
+        {
+            throw new Exception("区间未连接，无法取交集");
+        }
+
+        return new Interval<T>(
+            orderA.Start > orderB.Start ? orderA.Start : orderB.Start,
+            orderB.End > orderA.End ? orderA.End : orderB.End,
+            orderA.Start > orderB.Start ? orderA.ContainsStart : orderB.ContainsStart,
+            orderB.End > orderA.End ? orderA.ContainsEnd : orderB.ContainsEnd
+        );
+    }
+    
     
     public static Interval<T> operator +(Interval<T> interval, T value)
     {
@@ -86,13 +128,40 @@ public static class IntervalCal
 { 
     public static bool Contains(this double value, Interval<double> interval)
     {
-        return Interval<double>.Contains(interval, value);
+        return Interval<double>.IsContains(interval, value);
     }
     
     public static bool Contains(this Interval<double> interval, double value)
     {
-        return Interval<double>.Contains(interval, value);
+        return Interval<double>.IsContains(interval, value);
     }
+    
+    public static Interval<double> Union(this Interval<double> a, Interval<double> b)
+    {
+        return Interval<double>.Union(a, b);
+    }
+    
+    public static Interval<double> Intersect(this Interval<double> a, Interval<double> b)
+    {
+        return Interval<double>.Intersect(a, b);
+    }
+    
+    public static bool IsConnect(this Interval<double> a, Interval<double> b)
+    {
+        return Interval<double>.IsConnect(a, b);
+    }
+    
+    public static Interval<double> Reverse(this Interval<double> interval)
+    {
+        return Interval<double>.Reverse(interval);
+    }
+    
+    public static Interval<double> Order(this Interval<double> interval)
+    {
+        return Interval<double>.Order(interval);
+    }
+    
+    
 }
 
 
